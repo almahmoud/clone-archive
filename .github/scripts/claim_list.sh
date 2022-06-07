0bb4c322b02e4552ce2c9e54bf5fea2403806696
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2022-06-07 12:01:39 -0400

    Add set -x

--- .github/scripts/claim_list.sh
+++ .github/scripts/claim_list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 git pull origin main || git reset --hard origin/main
 export first=$(head -n 1 lists/todo.txt)
 mkdir -p lists/claimed/$(dirname $first) && mv lists/$first lists/claimed/$first && tail -n 100 -n +2 "lists/todo.txt" > "lists/todo.txt.tmp" && mv "lists/todo.txt.tmp" "lists/todo.txt"
--- .github/scripts/mark_done.sh
+++ .github/scripts/mark_done.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -x
 git pull origin main || git reset --hard origin/main
 mkdir -p lists/done/$(dirname $1) && mv lists/running/$1 lists/done/$1
 git add lists
